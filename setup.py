052ecc9bb5f67b9544a71aefa65d5a98552da043
Kelly Pierce <kellyannepierce@gmail.com>
2019-02-27 13:52:47 -0600

    version change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='workflow_tools',
-    version='0.1',
+    version='0.1.1',
     description='A tool for small workflows',
     url='https://github.com/kellyp2738/workflow_tools',
     author='Kelly Pierce',
